22d959a5f662f2977abe4b79e3a4d0fd04d7c4e5
evgeni.dimitrov <evgeni.dimitrov@nevexis.com>
2020-12-10 14:33:38 +0200

    add wiremock for testing

--- reactive-java/src/test/java/com/github/evgdim/reactive/ParallelTests.java
+++ reactive-java/src/test/java/com/github/evgdim/reactive/ParallelTests.java
@@ -1,18 +1,43 @@
 package com.github.evgdim.reactive;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.github.evgdim.reactive.model.Todo;
-import org.junit.jupiter.api.Test;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import org.junit.jupiter.api.*;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 import reactor.util.function.Tuple2;
 
-import java.time.Duration;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 
 public class ParallelTests {
+    private static WireMockServer wireMockServer = new WireMockServer(options().port(8089));
+
+    @BeforeEach
+    public void startMockServer() {
+        wireMockServer.start();
+    }
+    @AfterEach
+    public void stopMockServer() {
+        wireMockServer.stop();
+    }
+
     @Test
     public void parallelCallsMonoZip() {
-        WebClient webClient = WebClient.builder().baseUrl("https://jsonplaceholder.typicode.com").build();
+        wireMockServer.stubFor(get(urlEqualTo("/todos/1"))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                        .withBody("{ \"userId\": 1, \"id\": 1, \"title\": \"delectus aut autem\", \"completed\": false }")));
+        wireMockServer.stubFor(get(urlEqualTo("/todos/2"))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                        .withBody("{ \"userId\": 1, \"id\": 2, \"title\": \"delectus aut autem2\", \"completed\": false }")));
+
+        WebClient webClient = WebClient.builder().baseUrl(wireMockServer.baseUrl()).build();
         Mono<Todo> todoMono1 = webClient.get().uri("/todos/1").retrieve().bodyToMono(Todo.class);
         Mono<Todo> todoMono2 = webClient.get().uri("/todos/2").retrieve().bodyToMono(Todo.class);
 
